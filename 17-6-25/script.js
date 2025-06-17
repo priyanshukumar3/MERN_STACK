bbbc5159aaf15410257780c3673bf8583308e120
priyanshukumar3 <priyanshukumar3122@gmail.com>
2025-06-17 14:54:05 +0530

    till scope chain

---
+++ 17-6-25/script.js
@@ -0,0 +1,26 @@
+// console.log(this);
+// //declaration
+// var a;
+// //initialization
+// a = 10;
+// // Use
+// console.log(a);
+
+function get() {
+  var a;
+
+  a = 10;
+
+  if(true) {
+    let c = 20;
+
+    console.log(c);
+  }
+//   console.log(c);
+
+  console.log("inside: ", a);
+}
+
+get();
+
+// console.log("outside: ", a);
