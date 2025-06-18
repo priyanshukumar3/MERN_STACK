71fb925cc950b43f23545e4b41f22718f02b5327
priyanshukumar3 <priyanshukumar3122@gmail.com>
2025-06-18 14:48:36 +0530

    till operators

--- 18-6-25/script.js
+++ 18-6-25/script.js
@@ -92,46 +92,145 @@ func1();
 
 //NOTE - Objects
 
-let student = {
-  name: "Alice",
-  college: "LPU",
-  regId: 1234
-};
+// let student = {
+//   name: "Alice",
+//   college: "LPU",
+//   regId: 1234
+// };
 
-//Access:
-console.log(student.name);
+// //Access:
+// console.log(student.name);
 
-//Update:
-student.regId = 1256;
+// //Update:
+// student.regId = 1256;
 
-//NOTE - Arrays
+// //NOTE - Arrays
 
-let marks = [25, 28, 26, 30, "LPU"];
+// let marks = [25, 28, 26, 30, "LPU"];
 
-//Access:
-console.log(marks[0]);
-console.log(marks[1]);
-console.log(marks[2]);
-console.log(marks[3]);
-console.log(marks[4]);
-console.log(marks[6]);
+// //Access:
+// console.log(marks[0]);
+// console.log(marks[1]);
+// console.log(marks[2]);
+// console.log(marks[3]);
+// console.log(marks[4]);
+// console.log(marks[6]);
 
-//update
+// //update
 
-marks[0] = 21;
+// marks[0] = 21;
 
-console.log(marks);
+// console.log(marks);
 
-const students = new Array({ name: "alice" }, { name: "bob" });
+// const students = new Array({ name: "alice" }, { name: "bob" });
 
-console.log("a: ", students);
+// console.log("a: ", students);
 
-students.push({ name: "rohit" });
+// students.push({ name: "rohit" });
 
-console.log("b: ", students);
+// console.log("b: ", students);
 
-students.pop();
+// students.pop();
 
-console.log("pop: ", students);
+// console.log("pop: ", students);
 
-console.log("size: ", students.length);
+// console.log("size: ", students.length);
+
+
+// //NOTE - LOOPS
+
+// for (let i = 0; i < students.length; i++) {
+//   console.log("Student Name: ", students[i].name);
+// }
+
+// //While Loop
+
+// let j = 0; //initialization
+// //condition
+// while (i < students.length) {
+//   console.log("Student Name: ", students[i].name);
+
+//   j++; //updation
+// }
+
+// //Do-While Loop
+
+// //initialization
+// let k = 0;
+
+// do {
+//   //statement
+//   console.log("Student Name: ", students[k].name);
+
+//   k++; //updation
+// } while (k < students.length); //condition
+
+
+//  THERE ARE THREE TYPES OF FUNCTION :-
+// 1 named function
+// 2 arrow function
+// 3 annonymous function
+
+// function print() { // named
+//   console.log("LPU");
+// }
+
+// const print = () => { // arrow
+//   console.log("LPU");
+// }
+
+// const print = function () { // annonymous
+//   console.log("LPU");
+// }
+
+// print();
+
+
+
+
+//NOTE - Operators:
+
+/*
+1. Arithematic Operator
+
++, -, *, /, //, %
+
+2. Comparision Operator
+
+>, <, ==, ===, !=
+
+3. Assignment Operator
+=, +=, -=, *=, /=
+
+4. Ternary Operator
+
+expression ? statement: statement
+
+5. Logical Operator
+&&, ||, !, ?? 
+
+6. Bitwise Operator
+
+&, |, ^
+
+7. typeof Operator
+
+typeof
+
+*/
+
+let a = 3;
+let b = "3";
+
+console.log(a == b);
+console.log(a === b);
+
+console.log((a += 3)); //a = a+3
+
+console.log(a > 3 ? "Hello" : "Hi");
+
+a = null;
+
+console.log(a ?? "hello");
+
+console.log(typeof b);
---
+++ 18-6-25/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+on print() {
+  console.log("LPU");
+}
