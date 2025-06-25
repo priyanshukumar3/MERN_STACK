04bf461994bd06d9f2cb964dd4828a81e22a493b
priyanshukumar3 <priyanshukumar3122@gmail.com>
2025-06-25 14:54:55 +0530

    till react app

---
+++ React/first-app/src/components/Greet.js
@@ -0,0 +1,28 @@
+//components/Greet.js
+import React from "react";
+
+// export function Greet() {
+//   return <h1>Hello LPU</h1>;
+// }
+// export function Greet1() {
+//   return <h1>Hello Students</h1>;
+// }
+
+// function Greet() {
+//   return <h1>Hello Students</h1>;
+// }
+
+// export default Greet;
+
+
+
+function Greet(props) {
+  const { studentName, college, ...prop } = props;
+  return (
+    <h1>
+      Hello {studentName}, {college}, {prop.branch}
+    </h1>
+  );
+}
+
+export default Greet;
